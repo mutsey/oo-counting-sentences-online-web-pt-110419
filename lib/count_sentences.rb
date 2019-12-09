3772fdf3958c822335166a3a37254befab10701c
Meagan Utsey <meagan.utsey@gmail.com>
2019-12-09 02:44:40 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    "HI.".end_with?(".")
+  => true
   end
 
   def question?
-
+"HI?".end_with?("?")
+  => true
   end
 
   def exclamation?
-
+"HI!".end_with?("!")
+  => true
   end
 
   def count_sentences
-
+"HI!".end_with?("3")
+  => true
   end
 end
